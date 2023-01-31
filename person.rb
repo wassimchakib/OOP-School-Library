135fe2feb3714901542e59dd27ee0b294e262faf
Wassim <wassimchakib@hotmail.com>
2023-01-31 15:41:06 +0100

    Refactor Person to Nameable

--- person.rb
+++ person.rb
@@ -1,5 +1,8 @@
-class Person
+require './nameable'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -9,7 +12,11 @@ class Person
   attr_accessor :name, :age
 
   def can_use_services?
-    is_of_age && parent_permission
+    of_age && parent_permission
+  end
+
+  def correct_name
+    name
   end
 
   private
