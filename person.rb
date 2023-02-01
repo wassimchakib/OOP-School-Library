1ea2200f3128a4eb1fb79f7894fdd23e3a6cce94
Wassim <wassimchakib@hotmail.com>
2023-02-01 17:27:02 +0100

    Add Classroom class

--- person.rb
+++ person.rb
@@ -3,6 +3,9 @@ require './capitalize_decorator'
 require './trimmer_decorator'
 
 class Person < Nameable
+  attr_reader :id
+  attr_accessor :name, :age, :parent_permission
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..1000)
@@ -10,8 +13,6 @@ class Person < Nameable
     @age = age
     @parent_permission = parent_permission
   end
-  attr_reader :id
-  attr_accessor :name, :age
 
   def can_use_services?
     of_age && parent_permission
