b314c00937e9cacec7c42589addc8edff20f0768
Wassim <wassimchakib@hotmail.com>
2023-02-02 19:24:25 +0100

    Add main.rb file

--- app.rb
+++ app.rb
@@ -4,16 +4,20 @@ require './teacher'
 require './rental'
 
 class App
-  def list_all_books(books, show_index: true)
+  def list_all_books(books, show_index: false)
+    puts 'No books found' if books.empty?
     books.each_with_index do |book, index|
-      puts "#{"#{index}) " if show_index}Title: \"#{book.title}\", Author: #{book.author}" 
+      puts "#{"#{index}) " if show_index}Title: \"#{book.title}\", Author: #{book.author}"
     end
+    puts "\n"
   end
 
   def list_all_people(people, show_index: false)
+    puts 'No people found' if people.empty?
     people.each_with_index do |person, index|
       puts "#{"#{index}) " if show_index}[#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
+    puts "\n"
   end
 
   def list_all_rentals_for_a_person(id_person, persons)
@@ -35,15 +39,15 @@ class App
   end
 end
 
-app = App.new
-book1 = app.create_a_book('Atomic habits', 'James Clear')
-book2 = app.create_a_book('Millionaire Mindset', 'Simo Life')
-books = [book1, book2]
-app.list_all_books(books)
-person1 = app.create_a_person(1, 19, 'Wassim', nil, parent_permission: false)
-person2 = app.create_a_person(2, 35, 'Lord', 'Mathematics', parent_permission: true)
-persons = [person1, person2]
-app.list_all_people(persons)
-app.create_a_rental(book1, person2, '2022-09-23')
-app.create_a_rental(book2, person2, '2023-01-20')
-app.list_all_rentals_for_a_person(863, persons)
+# app = App.new
+# book1 = app.create_a_book('Atomic habits', 'James Clear')
+# book2 = app.create_a_book('Millionaire Mindset', 'Simo Life')
+# books = [book1, book2]
+# app.list_all_books(books)
+# person1 = app.create_a_person(1, 19, 'Wassim', nil, parent_permission: false)
+# person2 = app.create_a_person(2, 35, 'Lord', 'Mathematics', parent_permission: true)
+# persons = [person1, person2]
+# app.list_all_people(persons)
+# app.create_a_rental(book1, person2, '2022-09-23')
+# app.create_a_rental(book2, person2, '2023-01-20')
+# app.list_all_rentals_for_a_person(863, persons)
