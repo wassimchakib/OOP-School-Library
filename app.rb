4fe5cb30fd9e95a6f7ddbfdcac18e6f3342ad8cb
Wassim <wassimchakib@hotmail.com>
2023-02-02 19:27:15 +0100

    Delete inline comments

--- app.rb
+++ app.rb
@@ -38,16 +38,3 @@ class App
     Rental.new(person, book, date)
   end
 end
-
-# app = App.new
-# book1 = app.create_a_book('Atomic habits', 'James Clear')
-# book2 = app.create_a_book('Millionaire Mindset', 'Simo Life')
-# books = [book1, book2]
-# app.list_all_books(books)
-# person1 = app.create_a_person(1, 19, 'Wassim', nil, parent_permission: false)
-# person2 = app.create_a_person(2, 35, 'Lord', 'Mathematics', parent_permission: true)
-# persons = [person1, person2]
-# app.list_all_people(persons)
-# app.create_a_rental(book1, person2, '2022-09-23')
-# app.create_a_rental(book2, person2, '2023-01-20')
-# app.list_all_rentals_for_a_person(863, persons)
